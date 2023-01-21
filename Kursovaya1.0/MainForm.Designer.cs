bc048b1369aef5745b17e167802e72e89566244d
Andrey Zakharov <114531218+unluckyDustyk@users.noreply.github.com>
2023-01-21 04:21:15 +0300

    MainForm nachalo

---
+++ Kursovaya1.0/MainForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Kursovaya1._0
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WhereLabel = new System.Windows.Forms.Label();
+            this.ChooseWhereToGo = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // WhereLabel
+            // 
+            this.WhereLabel.AutoSize = true;
+            this.WhereLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.WhereLabel.Location = new System.Drawing.Point(12, 9);
+            this.WhereLabel.Name = "WhereLabel";
+            this.WhereLabel.Size = new System.Drawing.Size(151, 20);
+            this.WhereLabel.TabIndex = 0;
+            this.WhereLabel.Text = "Пункт назначения:";
+            // 
+            // ChooseWhereToGo
+            // 
+            this.ChooseWhereToGo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ChooseWhereToGo.FormattingEnabled = true;
+            this.ChooseWhereToGo.Location = new System.Drawing.Point(169, 8);
+            this.ChooseWhereToGo.Name = "ChooseWhereToGo";
+            this.ChooseWhereToGo.Size = new System.Drawing.Size(121, 26);
+            this.ChooseWhereToGo.TabIndex = 1;
+            this.ChooseWhereToGo.SelectedIndexChanged += new System.EventHandler(this.ChooseWhereToGo_SelectedIndexChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 461);
+            this.Controls.Add(this.ChooseWhereToGo);
+            this.Controls.Add(this.WhereLabel);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label WhereLabel;
+        private System.Windows.Forms.ComboBox ChooseWhereToGo;
+    }
+}
---
+++ Kursovaya1.0/MainForm.cs
@@ -0,0 +1,59 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Kursovaya1._0
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ChooseWhereToGo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //String city = ChooseWhereToGo.Items[ChooseWhereToGo.SelectedIndex].ToString();
+
+            DataBase db = new DataBase();
+            
+            DataSet ds = new DataSet();
+
+            //DataTable table = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `schedule` WHERE `whereToGo` = @wTG AND `departudeDate` = @dD AND 'tranferDate' = @tD AND 'arrivalDate' = @aD AND 'availableSeats' = @aS AND 'cost' = @co", db.getConnection());
+            //command.Parameters.Add("@wTG", MySqlDbType.VarChar).Value = city;
+
+            //adapter.SelectCommand = command;
+            adapter.Fill(ds);
+
+            int i = 0;
+            for (i = 0; i < ds.Tables["schedule"].Rows.Count; i++) // перебираем все строки
+            {
+                // выводим в комбобокс
+                ChooseWhereToGo.Items.Add(ds.Tables["schedule"].Rows[i]["whereToGo"].ToString());
+            }
+        }
+        
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
